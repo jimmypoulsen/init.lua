4a860b4351154519180f2e6ab200f619a67e7da2
Jimmy Poulsen <jimmypoulsen96@gmail.com>
2024-12-09 23:39:08 +0100

    Update packages

---
+++ lua/theprimeagen/lazy/tree.lua
@@ -0,0 +1,14 @@
+-- Disabled for now. Don't need it.
+-- return {
+--   "nvim-tree/nvim-tree.lua",
+--   version = "*",
+--   lazy = false,
+--   dependencies = {
+--     "nvim-tree/nvim-web-devicons",
+--   },
+--   config = function()
+--     require("nvim-tree").setup {}
+--   end,
+-- }
+
+return {}
