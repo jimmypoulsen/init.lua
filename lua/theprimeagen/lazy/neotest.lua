6c2d55d1958f3e1b91a2eaa99304ea61a78b62d4
Jimmy Poulsen <jimmypoulsen96@gmail.com>
2025-04-22 14:38:51 +0200

    Configure neotest for RSpec

--- lua/theprimeagen/lazy/neotest.lua
+++ lua/theprimeagen/lazy/neotest.lua
@@ -8,12 +8,14 @@ return {
             "nvim-treesitter/nvim-treesitter",
             "marilari88/neotest-vitest",
             "nvim-neotest/neotest-plenary",
+            "olimorris/neotest-rspec",
         },
         config = function()
             local neotest = require("neotest")
             neotest.setup({
                 adapters = {
                     require("neotest-vitest"),
+                    require("neotest-rspec"),
                     require("neotest-plenary").setup({
                         -- this is my standard location for minimal vim rc
                         -- in all my projects
@@ -24,6 +26,18 @@ return {
 
             vim.keymap.set("n", "<leader>tc", function()
                 neotest.run.run()
+            end)
+
+            vim.keymap.set("n", "<leader>tC", function()
+                neotest.run.run(vim.fn.expand("%"))
+            end)
+
+            vim.keymap.set("n", "<leader>ts", function()
+                neotest.summary.toggle()
+            end)
+
+            vim.keymap.set("n", "<leader>tv", function()
+              neotest.output_panel.toggle()
             end)
         end,
     },
